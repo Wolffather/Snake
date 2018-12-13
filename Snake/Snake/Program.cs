e47182e60881fffa2d87408bbf0611414e0adf72
Saveliy Mirskov <Saveliy.Mirskov@dataart.com>
2018-12-13 14:20:57 +0300

    Second attempt

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,26 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Here we go again");
+            int x1, y1;
+            char sym1 = '*';
+            Random rnd = new Random();
+
+            for (int i = 0; i < 25; i++)
+            {
+                x1 = rnd.Next(1, 30);
+                y1 = rnd.Next(1, 30);
+                dotDrawer(x1, y1, sym1);
+
+            }
+
             Console.ReadKey();
         }
+
+        static void dotDrawer(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
     }
 }
